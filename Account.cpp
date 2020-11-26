a4c64fc2f47e8c2a1bd959615a0b48bcca7214b1
Dominic Zucchini <dom14@live.missouristate.edu>
2020-11-26 13:51:59 -0600

    Separated AccountType and Account. Defined setters and getters.

--- Account.cpp
+++ Account.cpp
@@ -3,3 +3,170 @@
 
 using namespace std;
 
+/**********************************************************
+/ AccountType
+*//////////////////////////////////////////////////////////
+/**********************************************************
+/ Setters
+*//////////////////////////////////////////////////////////
+void AccountType::setMonthlyFee(double fee)
+{
+    monthlyFee = fee;
+}
+
+void AccountType::setServiceFee(double fee)
+{
+    serviceFee = fee;
+}
+
+bool AccountType::setInterestRate(double rate)
+{
+    if(rate > 0 && rate < 5.0){
+        interestRate = rate;
+        return true;
+    }
+    else
+        return false;
+}
+
+/**********************************************************
+/ Getters
+*//////////////////////////////////////////////////////////
+string Account::getAccountHolderInfo()
+{
+    return accountHolderFirstName+" "+accountHolderLastName+" "+accountHolderPhoneNumber+" "+accountHolderAddress;
+}
+
+string Account::getAccountNumber()
+{
+    return accountNumber;
+}
+
+double AccountType::getMonthlyFee()
+{
+    return monthlyFee;
+}
+
+double AccountType::getServiceFee()
+{
+    return serviceFee;
+}
+
+double AccountType::getInterestRate()
+{
+    return interestRate;
+}
+
+/**********************************************************
+/ Account
+*//////////////////////////////////////////////////////////
+/**********************************************************
+/ Setters
+*//////////////////////////////////////////////////////////
+void Accout::setAccountHolderFirstName(string name)
+{
+    // possibly add check for valid inputs
+    accountHolderFirstName = name;
+}
+
+void Account::setAccountHolderLastName(string name)
+{
+    // possibly add check for valid inputs
+    accountHolderLastName = name;
+}
+
+void Account::setAccountHolderPhoneNumber(string phoneNum)
+{
+    // possibly add check for valid inputs
+    accountHolderPhoneNumber = phoneNum;
+}
+
+void Account::setAccountHolderAddress(string address)
+{
+    // possibly add check for valid inputs
+    accountHolderAddress = address;
+}
+
+void Account::setCloseDate(time_t date)
+{
+    closeDate = date;
+}
+
+void Account::setAccountBalance(double amount)
+{
+    accountBalance = roundNum(amount, 2);
+}
+
+void Account::setRestrictedStatus(bool status)
+{
+    restrictedStatus = status;
+}
+
+void Account::setOpenStatus(bool status)
+{
+    openStatus = status;
+}
+
+/**********************************************************
+/ Getters
+*//////////////////////////////////////////////////////////
+time_t Account::getOpenDate()
+{
+    return openDate;
+}
+
+time_t Account::getMaturityDate()
+{
+    return maturityDate;
+}
+
+time_t Account::getTermLength()
+{
+    return maturityDate - openDate;
+}
+
+double Account::getAccountBalance()
+{
+    return accountBalance;
+}
+
+bool Account::getRestrictedStatus()
+{
+    return restrictedStatus;
+}
+
+bool Account::getOpenStatus()
+{
+    return openStatus;
+}
+
+/**********************************************************
+/ roundNum (protected) rounds a floating point number to
+/ a specified number of decimals
+/
+/ parameters:
+/   amount : double, precision : int
+/
+/ returns:
+/   double
+*//////////////////////////////////////////////////////////
+double Account::roundNum(double amount, int precision)
+{
+    // implementation by nathan
+}
+
+/**********************************************************
+/ convertTime (protected) converts the ctime integer time_t
+/ to a string of the exact date in the format:
+/   "<day of the week> <month> <day> <time> <year>"
+/
+/ parameters:
+/   inputTime : time_t
+/
+/ returns:
+/   string
+*//////////////////////////////////////////////////////////
+string Account::convertTime(time_t inputTime)
+{
+    return ctime(&inputTime);
+}
--- Account.h
+++ Account.h
@@ -5,23 +5,43 @@
 #include <ctime>
 using namespace std;
 
-class Account
+class AccountType
+{
+private:
+    string accountNumber;
+    double monthlyFee;
+    double serviceFee;
+    static double penaltyFee;
+    double interestRate;
+    double minimumBalance;
+
+public:
+    void setAccountHolderFirstName(string name);
+    void setAccountHolderLastName(string name);
+    void setAccountHolderPhoneNumber(string phoneNum);
+    void setAccountHolderAddress(string address);
+    string getAccountHolderInfo();
+    string getAccountNumber();
+    void setMonthlyFee(double fee);
+    double getMothlyFee();
+    void setServiceFee(double fee);
+    double getServiceFee();
+    bool setInterestRate(double rate);
+    double getInterestRate();
+
+};
+
+class Account : public AccountType
 {
 private:
     string accountHolderFirstName;
     string accountHolderLastName;
     string accountHolderPhoneNumber;
     string accountHolderAddress;
-    string accountNumber;
-    double monthlyFee;
-    double serviceFee;
-    static double penaltyFee;
-    double interestRate;
     time_t openDate;
     time_t closeDate;
     time_t maturityDate;
     double accountBalance;
-    double minimumBalance;
     bool restrictedStatus;
     bool openStatus;
 
@@ -38,18 +58,15 @@ public:
     void setAccountHolderPhoneNumber(string phoneNum);
     void setAccountHolderAddress(string address);
     string getAccountHolderInfo();
-    string getAccountNumber();
-    void setMonthlyFee(double fee);
-    double getMothlyFee();
-    void setServiceFee(double fee);
-    double getServiceFee();
-    bool setInterestRate(double rate);
-    double getInterestRate();
     time_t getOpenDate();
     time_t setCloseDate(); // same as just closing?
     time_t getMaturityDate();
     time_t getTermLength();
     double getAccountBalance();
+    void setRestrictedStatus(bool status);
+    void setOpenStatus(bool status);
+    bool getRestrictedStatus();
+    bool getOpenStatus();
     string deposit(double amount); // returns string of what happened
     string withdraw(double amount);
     void displayHistory(string beginning, string ending);
