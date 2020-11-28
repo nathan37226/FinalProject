d10a243e0bfc54b6072d6fbe1042b71d465b96b4
Nathan Obert <nathan829@live.missouristate.edu>
2020-11-28 16:11:16 -0600

    Functions related to finding days since a unix time + demo

--- HelperFunctions.cpp
+++ HelperFunctions.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <ctime>
 using namespace std;
 
 double roundNum(double value, int precision);
 string getDisplayNum(double input);
 string incrementAcctNum(string lastActNum);
+int daysElapsed(time_t &previousTime);
+time_t midnightTimeStamp(time_t curTime);
 
 int main()
 {
@@ -17,6 +20,16 @@ int main()
     double roundedNum = roundNum(num, 2); //for money, we round to the 2nd decimal; if you write this number to a file, it wll have 105.690000, so need to format it!
     string formattedNum = getDisplayNum(roundedNum);
     cout << "Original num: " << num << endl << "Rounded num: " << roundedNum << endl << "Num formatted to display: $" << formattedNum << endl;
+    
+    cout << endl;
+    time_t nov26 = 1606348800;
+    time_t curTime = time(0);
+    time_t midnightOfCurDay = midnightTimeStamp(curTime);
+    int daysSinceNov26 = daysElapsed(nov26);
+    cout << "Nov26 in unix: " << nov26 << ". Current time in unix: " << curTime << endl;
+    cout << "Midnight of current day: " << midnightOfCurDay << ". Days since Nov 26: " << daysSinceNov26 << endl;
+    
+    
     return 0;
 }
 
@@ -58,4 +71,27 @@ string incrementAcctNum(string lastAcctNum)
     newAcctNum = accountPrefix + newAcctNum; //adding prefix back
 
     return newAcctNum;
+}
+
+//finding time elapsed since last interest application
+int daysElapsed(time_t &previousTime)
+{
+    time_t currentTime = time(0);
+    time_t secondsElapsed = currentTime - previousTime;
+
+    int days = secondsElapsed / 86400; //86400 seconds in a day, and the int type of day will round like a floor function;
+    return days;
+}
+
+//tail-recursive function to return unix timestamp of current time at midnight, used for timestamp on .txt file
+time_t midnightTimeStamp(time_t curTime)
+{
+    if (curTime <= 86400)
+    {
+        return (time(0) - curTime); //returns how many seconds have elapsed in the current day
+    }
+    else
+    {  
+        return midnightTimeStamp(curTime-86400); //subtracts one day off
+    }
 }
