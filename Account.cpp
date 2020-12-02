399f3f6eda290713e796f6bb4629a27942c83681
Dominic Zucchini <dom14@live.missouristate.edu>
2020-12-01 19:55:18 -0600

    Removed main

--- Account.cpp
+++ Account.cpp
@@ -10,21 +10,6 @@ using namespace std;
 void accountInit();
 void accountExit();
 
-int main()
-{
-    accountInit();
-    cout << endl;
-    //Account a("checking","dom14","Dominic","Zucchini","417-551-2422","1234 N Street Place Springfield, MO, 65805",0,0.0);
-    Account a("C000000001");
-    a.deposit(200.001);
-    a.withdraw(100.5);
-    cout << a.getAccountHolderUserID() << endl;
-    cout << Account::getDisplayNum(a.getAccountBalance()) << endl;
-    a.displayHistory("12/01/2020 00:00:00", "12/02/2020 00:00:00");
-    cout << a.saveToFile() << endl;
-    accountExit();
-}
-
 void accountInit()
 {
     cout << "Creating default account types..." << endl;
